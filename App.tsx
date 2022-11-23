70714d5131c7e85e00c632286716c2533c34b563
MatheusFoliatti <matheusfoliatti@gmail.com>
2022-11-22 22:27:32 -0300

    screen

--- App.tsx
+++ App.tsx
@@ -1,27 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-    <Text style={styles.title}>Hello World</Text>
-    <Text style={styles.paragraph}>With Expo.io</Text>
-    </View>
+  
+  return (      
+    
+    
+    <LinearGradient
+      colors={['#63CAC6', '#1E61B0']}
+      style={styles.container}>
+    
+    <FontAwesome5 name="user-circle" 
+    size={130} 
+    color={'white'} 
+    solid
+    style={styles.icon}
+    />
+
+    <View style={styles.label}>
+     
+      <FontAwesome5 name="user" 
+      size={25} 
+      color={'#0798cc'} 
+      light
+      style={styles.icon}
+      />
+     
+      <Text
+      style={styles.text} > Username </Text> 
+      
+      </View>
+    
+    </LinearGradient>
+    
+    
   );
 }
+
+//'#40f2fc'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#642eaa',
     alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+    height: '100%'
   },
-  title: {
-    fontSize:28
-  },
-  paragraph: {
-    fontSize:15
-  }
+  text: {
+    backgroundColor: '#40f2fc',
+    color: '#0798cc',
+    fontSize: 20,
+    borderRadius: 20,
+    padding: 5,
+    width: 200,
+    textAlign: 'center',
+    marginTop: 45,
+    fontWeight: 'bold'
 
+  },
+  icon: {
+    paddingTop: 100,
+    position:'relative'
+  },
+  button:{
+    paddingTop: 50,
+  },
+  label:{
+    display: 'flex',
+    flex: 1,
+  }
 });
