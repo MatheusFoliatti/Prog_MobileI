a9ff9078b2b00922d575d6cb6bc58faf50da6803
MatheusFoliatti <matheusfoliatti@gmail.com>
2022-11-29 20:38:59 -0300

    done

--- App.tsx
+++ App.tsx
@@ -1,75 +1,160 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, Image, TextInput } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import React from 'react';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+import * as React from 'react';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
-
-export default function App() {
-  
-  return (      
-    
-    
-    <LinearGradient
-      colors={['#63CAC6', '#1E61B0']}
-      style={styles.container}>
-    
-    <FontAwesome5 name="user-circle" 
-    size={130} 
-    color={'white'} 
-    solid
-    style={styles.icon}
-    />
-
-    <View style={styles.label}>
-     
-      <FontAwesome5 name="user" 
-      size={25} 
-      color={'#0798cc'} 
-      light
-      style={styles.icon}
+const App = () => {
+  return (
+    <LinearGradient colors={['#63CAC6', '#1E61B0']} style={styles.container}>
+      <FontAwesome5 
+        style={styles.userIcon}
+        solid
+        name='user-circle' 
+        size={130} 
+        color='white'
       />
-     
-      <Text
-      style={styles.text} > Username </Text> 
-      
+      <View style={styles.input}>
+        <FontAwesome5 
+          name='user' 
+          size={20} 
+          color='#008BB2'
+        />
+        <Text style={styles.inputText}>Username</Text>
       </View>
-    
+      <View style={styles.input}>
+        <FontAwesome5 
+          name='lock' 
+          size={20} 
+          color='#008BB2'
+        />
+        <Text style={styles.inputText}>Password</Text>
+      </View>
+      
+      <View style={styles.inputLogin}>
+        <Text style={styles.inputTextLogin}>Login Now</Text>
+      </View>
+
+      <View style= {styles.extraInput}>
+  
+        <Text style= {styles.extraText1}>     
+        <FontAwesome5 
+          name='check-square' 
+          size={13} 
+          color='#fff'
+        /> Remember me </Text>  
+        <Text style= {styles.extraText2}>Forgot password?</Text>  
+      </View>
+      
+      <View style= {styles.endPage}>
+        <Text style= {styles.member}> Not a Member? </Text>
+        <View style={styles.inputSmall}> 
+          <Text style={styles.member}> Create Account </Text> 
+        </View>
+      </View>
+
+      <StatusBar style="auto" />
     </LinearGradient>
-    
-    
   );
 }
-
-//'#40f2fc'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    width: '100%',
-    height: '100%'
+    gap: '25px'
+  },
+  userIcon: {
+    marginTop: 80,
+    marginBottom: 15,
   },
   text: {
-    backgroundColor: '#40f2fc',
-    color: '#0798cc',
-    fontSize: 20,
-    borderRadius: 20,
-    padding: 5,
-    width: 200,
-    textAlign: 'center',
-    marginTop: 45,
-    fontWeight: 'bold'
-
+    marginBottom: 50,
+    color: 'red',
+    fontSize: 20
+    
   },
-  icon: {
-    paddingTop: 100,
-    position:'relative'
+  inputText: {
+    fontSize:19,
+    color:'#008BB2',
+    fontStyle: undefined,
+    fontWeight:'bold',
+    marginLeft:'50px'
   },
-  button:{
-    paddingTop: 50,
-  },
-  label:{
+  input: {
+    backgroundColor: '#40F2FC',
+    borderRadius: 100,
+    width: 250,
+    height:50,
+    alignItems: 'center',
     display: 'flex',
-    flex: 1,
+    flexDirection: 'row',
+    paddingLeft: '25px',
+    fontWeight :'bold',
+  },
+  inputLogin: {
+    borderRadius: 100,
+    width: 250,
+    height:50,
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'row',
+    paddingLeft: '25px',
+    fontWeight :'bold',
+    backgroundColor: '#f8b4a8',
+  },
+  inputTextLogin: {
+    color:'white',
+    fontSize:19,
+    fontStyle: undefined,
+    fontWeight:'bold',
+    marginLeft:'50px'
+  },
+  extraInput: {
+    display:'flex',
+    flexDirection:'row',
+    gap: '35px'
+  },
+  extraText1: {
+    fontSize:13,
+    color:'white',
+    fontWeight:'bold'
+  },
+  extraText2: {
+    fontSize:13,
+    color:'white',
+    fontWeight:'bold',
+    borderBottomColor: '#fff',
+    borderBottomWidth: 2
+  },
+  member:{
+    fontSize:11,
+    color:'white',
+    fontWeight:'bold',
+    textAlign:'center',
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    padding:'1px'
+  },
+  inputSmall: {
+    borderRadius: 100,
+    width: 125,
+    height:15,
+    alignItems: 'center',
+    display: 'flex',
+    fontWeight :'bold',
+    backgroundColor: 'transparent',
+    outlineColor: "white",
+    outlineStyle: "solid",
+    outlineWidth: 2,
+  },
+  endPage:{
+    paddingTop:'50px',
+    gap:'3px'
   }
+
 });
+
+export default App
